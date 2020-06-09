4cd8455cb5de781b7e7a56b9c1e89234618e1e1d
Kim Fisher <kfisher@wcs.org>
2020-06-09 13:11:10 -0400

    refactor to use precalculated gpw

--- src/hii_landuse.py
+++ src/hii_landuse.py
@@ -7,25 +7,25 @@ from task_base import EETask
 class HIILanduse(EETask):
     ee_rootdir = "projects/HII/v1/sumatra_poc"
     ee_driverdir = "driver/landuse"
-    # if input lives in ee, it should have an "ee_path" pointing to an ImageCollection/FeatureCollection
     inputs = {
         "gpw": {
             "ee_type": EETask.IMAGECOLLECTION,
-            "ee_path": "CIESIN/GPWv411/GPW_Population_Density",
-            "maxage": 5,  # years
+            "ee_path": f"{ee_rootdir}/misc/gpw_interpolated",
+            "maxage": 1,
         },
-        # TODO: This should be an image collection, and in projects/HII/v1/source/phys
+        # I replaced source in aduncan/ with projects/HII/v1/source/phys and temporarily assigned 1992-01-01 00:00:00
         # TODO: Replace source? How do we calculate beyond 2015? How do we calculate at a resolution < 1 year?
-        # Also this failed with taskdate=2010-01-01 because "Pattern 'b29' did not match any bands"
+        #  Also this failed with taskdate=2010-01-01 because "Pattern 'b29' did not match any bands"
         "esacci": {
             "ee_type": EETask.IMAGE,
-            "ee_path": "users/aduncan/cci/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v207",
+            "ee_path": "projects/HII/v1/source/phys/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v207",
+            "maxage": 30,
         },
-        # TODO: make sure this has a system:start_date or whatever we choose as reliable timestamp
+        # TODO: assign actual date to this source. I temporarily assigned 1992-01-01 00:00:00.
         "watermask": {
             "ee_type": EETask.IMAGE,
             "ee_path": "projects/HII/v1/source/phys/watermask_jrc70_cciocean",
-            "maxage": 20,
+            "maxage": 30,
         },
     }
     scale = 300
@@ -36,28 +36,19 @@ class HIILanduse(EETask):
         self.set_aoi_from_ee("{}/sumatra_poc_aoi".format(self.ee_rootdir))
 
     def calc(self):
-        gpw = ee.ImageCollection(self.inputs["gpw"]["ee_path"])
+        gpw, gpw_date = self.get_most_recent_image(ee.ImageCollection(self.inputs["gpw"]["ee_path"]))
         watermask = ee.Image(self.inputs["watermask"]["ee_path"])
         esacci = ee.Image(self.inputs["esacci"]["ee_path"])
         ee_taskdate = ee.Date(self.taskdate.strftime(self.DATE_FORMAT))
+        # TODO: see above -- need a source that will work after 2015
         year = ee_taskdate.get("year")
+        if year.getInfo() > 2015:
+            year = ee.Number(2015)
+        else:
+            year = ee.Number(year)
 
-        gpw_prior = gpw.filterDate(
-            ee_taskdate.advance(-self.gpw_cadence, "year"), ee_taskdate
-        ).first()
-        gpw_later = gpw.filterDate(
-            ee_taskdate, ee_taskdate.advance(self.gpw_cadence, "year")
-        ).first()
-        gpw_diff = gpw_later.subtract(gpw_prior)
-        numerator = ee_taskdate.difference(gpw_prior.date(), "day")
-        gpw_diff_fraction = gpw_diff.multiply(numerator.divide(self.gpw_cadence * 365))
-        gpw_taskdate = gpw_prior.add(gpw_diff_fraction)
-        gpw_taskdate_300m = gpw_taskdate.resample().reproject(
-            crs=self.crs, scale=self.scale
-        )
-
-        popdens_oneplus = gpw_taskdate_300m.gte(1)
-        yearband = ee.String("b").cat(ee.Number(year).subtract(1991).format())
+        popdens_oneplus = gpw.gte(1)
+        yearband = ee.String("b").cat(year.subtract(1991).format())
         # TODO: once esacci is an image collection, use self.get_most_recent_image()
         # TODO: replace hardcoded arguments like multiply(10) with variables
         esacci_oneyear = esacci.select(yearband).multiply(popdens_oneplus.multiply(10))
@@ -74,7 +65,6 @@ class HIILanduse(EETask):
 
     def check_inputs(self):
         super().check_inputs()
-        # add any task-specific checks here, and set self.status = self.FAILED if any fail
 
 
 if __name__ == "__main__":
