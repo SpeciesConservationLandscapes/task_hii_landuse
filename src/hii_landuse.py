20880ec4c2b80feae58181846e07bcd314109796
aduncan <aduncan@wcs.org>
2020-06-28 09:35:05 -0700

    followed #todos

--- src/hii_landuse.py
+++ src/hii_landuse.py
@@ -13,19 +13,20 @@ class HIILanduse(EETask):
             "ee_path": f"{ee_rootdir}/misc/gpw_interpolated",
             "maxage": 1,
         },
-        # I replaced source in aduncan/ with projects/HII/v1/source/phys and temporarily assigned 1992-01-01 00:00:00
         # TODO: Replace source? How do we calculate beyond 2015? How do we calculate at a resolution < 1 year?
-        #  Also this failed with taskdate=2010-01-01 because "Pattern 'b29' did not match any bands"
+        # (Adams note: Now an image collection to 2018. I don't think it will be possible to calculate at a resolution < 1 year?)
+
         "esacci": {
-            "ee_type": EETask.IMAGE,
-            "ee_path": "projects/HII/v1/source/phys/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v207",
-            "maxage": 30,
+            "ee_type": EETask.IMAGECOLLECTION,
+            "ee_path": "projects/HII/v1/source/lc/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v207",
+            "maxage": 3,
         },
         # TODO: assign actual date to this source. I temporarily assigned 1992-01-01 00:00:00.
+        # (Adam's note: jrc70 means water occurred there at least 70% of the time, from  16 March 1984 and 10 October 2015... I've set it to 16 March 1984; is that ok?)
         "watermask": {
             "ee_type": EETask.IMAGE,
             "ee_path": "projects/HII/v1/source/phys/watermask_jrc70_cciocean",
-            "maxage": 30,
+            "maxage": 50,
         },
     }
     scale = 300
@@ -37,28 +38,44 @@ class HIILanduse(EETask):
 
     def calc(self):
         gpw, gpw_date = self.get_most_recent_image(ee.ImageCollection(self.inputs["gpw"]["ee_path"]))
+        esacci, esacci_date = self.get_most_recent_image(ee.ImageCollection(self.inputs["esacci"]["ee_path"]))
         watermask = ee.Image(self.inputs["watermask"]["ee_path"])
-        esacci = ee.Image(self.inputs["esacci"]["ee_path"])
         ee_taskdate = ee.Date(self.taskdate.strftime(self.DATE_FORMAT))
-        # TODO: see above -- need a source that will work after 2015
-        year = ee_taskdate.get("year")
-        if year.getInfo() > 2015:
-            year = ee.Number(2015)
-        else:
-            year = ee.Number(year)
+
 
         popdens_oneplus = gpw.gte(1)
-        yearband = ee.String("b").cat(year.subtract(1991).format())
-        # TODO: once esacci is an image collection, use self.get_most_recent_image()
-        # TODO: replace hardcoded arguments like multiply(10) with variables
-        esacci_oneyear = esacci.select(yearband).multiply(popdens_oneplus.multiply(10))
 
-        esacci_remap_input = [10, 11, 12, 20, 30, 40, 190, 1000, 1100, 1200, 1210, 1220, 1300]
-        esacci_remap_output = [7, 7, 7, 8, 6, 4, 10, 4, 4, 4, 4, 4, 4]
-        hii_landuse_driver = esacci_oneyear.remap(
-            esacci_remap_input, esacci_remap_output, 0
-        ).updateMask(watermask)
+        esacci_10 = esacci.eq(10) # Cropland, rainfed
+        esacci_11 = esacci.eq(11) # Cropland, rainfed - Herbaceous cover
+        esacci_12 = esacci.eq(12) # Cropland, rainfed - Tree or shrub cover
+        esacci_20 = esacci.eq(20) # Cropland,irrigated or post‐flooding
+        esacci_30 = esacci.eq(30) # Mosaic cropland (>50%) / natural vegetation (tree, shrub, herbaceous cover) (<50%)
+        esacci_40 = esacci.eq(40) # Mosaic natural vegetation (tree, shrub, herbaceous cover) (>50%) / cropland (<50%)
+        esacci_190 = esacci.eq(190) # Urban areas
 
+        # note that since all of Sumatra is popdens <1 this always applies... maybe increase threshold? 
+        esacci_100 = esacci.eq(100).multiply(popdens_oneplus) # Mosaic tree and shrub (>50%) / herbaceous cover (<50%)
+        esacci_110 = esacci.eq(110).multiply(popdens_oneplus) # Mosaic herbaceous cover (>50%) / tree and shrub (<50%)
+        esacci_120 = esacci.eq(120).multiply(popdens_oneplus) # Shrubland
+        esacci_121 = esacci.eq(121).multiply(popdens_oneplus) # Shrubland - Evergreen shrubland
+        esacci_122 = esacci.eq(122).multiply(popdens_oneplus) # Shrubland - Deciduous shrubland
+        esacci_130 = esacci.eq(130).multiply(popdens_oneplus) # Grassland
+
+
+        hii_landuse_driver = esacci_10.multiply(7)\
+                                .add(esacci_11.multiply(7))\
+                                .add(esacci_12.multiply(7))\
+                                .add(esacci_20.multiply(8))\
+                                .add(esacci_30.multiply(6))\
+                                .add(esacci_40.multiply(4))\
+                                .add(esacci_190.multiply(10))\
+                                .add(esacci_100.multiply(4))\
+                                .add(esacci_110.multiply(4))\
+                                .add(esacci_120.multiply(4))\
+                                .add(esacci_121.multiply(4))\
+                                .add(esacci_122.multiply(4))\
+                                .add(esacci_130.multiply(4))\
+                                .updateMask(watermask)
         self.export_image_ee(
             hii_landuse_driver, "{}/{}".format(self.ee_driverdir, "hii_landuse_driver")
         )
